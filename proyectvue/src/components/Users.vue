70b4e891f931acc646db23ae562929b61a5c1f2c
guillo <glego40_@hotmail.com>
2017-04-01 09:47:02 -0500

    vue dojo

---
+++ proyectvue/src/components/Users.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+<div>
+	<p>Vista users</p>
+	<ul v-for="user in users">
+		<li>{{user.name}}</li>
+	</ul>
+</div>
+</template>
+
+
+<script>
+import axios from 'axios'
+export default{
+	data(){
+		return{
+			users:[]
+
+	    }
+
+	},
+
+	mounted(){
+		axios.get("http://jsonplaceholder.typicode.com/users")
+		.then(response => this.users = response.data)
+	}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ proyectvue/src/components/nombre.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+	<p>Stefanny T Ramirez</p>
+</template>
+
+
+<script>
+export default{
+	
+}
+</script>
+
+<style lang="css">
+</style>
